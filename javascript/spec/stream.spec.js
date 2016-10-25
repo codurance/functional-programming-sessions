d7863c2155f25e66c36f3423be8dc8e90248477d
tobyret <toby.retallick@gmail.com>
2016-10-25 19:46:48 +0100

    Green: finite stream

--- javascript/spec/stream.spec.js
+++ javascript/spec/stream.spec.js
@@ -6,14 +6,14 @@ chai.config.includeStack = true;
 
 describe('A stream of natural numbers', () => {
   it('returns the first natural number', () => {
-    expect(stream().value()).to.equal(1);
+    expect(infiniteStreamOfNaturalNumbers().value()).to.equal(1);
   });
 
   it('returns the second natural numbers', () => {
-    expect(stream().next().value()).to.equal(2);
-    expect(get(stream(), 1)).to.equal(1);
-    expect(get(stream(), 2)).to.equal(2);
-    expect(get(stream(), 3)).to.equal(3);
+    expect(infiniteStreamOfNaturalNumbers().next().value()).to.equal(2);
+    expect(get(infiniteStreamOfNaturalNumbers(), 1)).to.equal(1);
+    expect(get(infiniteStreamOfNaturalNumbers(), 2)).to.equal(2);
+    expect(get(infiniteStreamOfNaturalNumbers(), 3)).to.equal(3);
   });
 
   it('converts a function into a stream', () => {
@@ -24,9 +24,50 @@ describe('A stream of natural numbers', () => {
     // expect(get(streamCreator((n) => 2 * n, 1)), 2).to.equal(2);
     // expect(get(streamCreator((n) => 2 * n, 1)), 4).to.equal(8);
   });
+
+  describe('finite stream of natural numbers', () =>{
+    let stream;
+    beforeEach(() => {
+      stream = finiteStreamOfNaturalNumbers(4);
+    });
+    it('case', () => {
+      expect(stream.next().value()).to.equal(2);
+    });
+    it('case', () => {
+      expect(get(stream, 1)).to.equal(1);
+    });
+    it('case', () => {
+      expect(get(stream, 2)).to.equal(2);
+    });
+    it('case', () => {
+      expect(get(stream, 4)).to.equal(4);
+    });
+    it('case', () => {
+      const lastValueOfTheStream = stream.next().next().next();
+      expect(lastValueOfTheStream.next()).to.equal(undefined);
+    });
+  });
 });
 
-function stream() {
+function finiteStreamOfNaturalNumbers(numberOfElements) {
+  const stream_= function (current, numberOfPendingElements) {
+    return {
+      value: function () {
+        return current;
+      },
+      next: function () {
+        if (numberOfPendingElements === 0) {
+          return undefined;
+        }
+        return stream_(current + 1, numberOfPendingElements - 1);
+      }
+    };
+  };
+
+  return stream_(1, numberOfElements - 1);
+}
+
+function infiniteStreamOfNaturalNumbers() {
   const stream_= function (current) {
     return {
       value: function () {
@@ -65,10 +106,10 @@ function stream() {
 // }
 
 function get(stream, ordinal) {
-  var xx = range(ordinal - 1);
+  const xx = range(ordinal - 1);
   return xx.reduce(advanceStream, stream).value();
 
-  function advanceStream (stream) {
+  function advanceStream(stream) {
     return stream.next();
   }
 
