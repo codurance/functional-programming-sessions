e2d7d6952900c60cdc4abf709e7f0f7a78ce18a9
Alvaro Garcia <derecerca@gmail.com>
2016-11-04 22:07:45 +0000

    Refactor: use pattern matching

--- javascript/src/streem.js
+++ javascript/src/streem.js
@@ -13,13 +13,11 @@ class InfiniteNaturalStream {
   }
 
   take(n) {
-    const take_ = funct((result, n) => {
-      if (n === 0) {
-        return result;
-      } else {
-        return result.concat([this.value]).concat(this.next().take(n-1));
-      }
-    });
+    const take_ = funct((result, n) =>
+      match(n, {result, n},
+        (n) => n === 0, ({result}) =>  result,
+        () => true, ({result, n}) => result.concat([this.value]).concat(this.next().take(n-1))
+    ));
     return take_([], n);
   }
 }
@@ -35,4 +33,28 @@ function infiniteNaturals() {
  */
 function funct(lambda) {
   return lambda.bind(this);
+}
+
+function match(toMatchOn, parameters, ...equationParts) {
+  const equations = pair(equationParts);
+  for (let i = 0; i < equations.length; i++) {
+    const equation = equations[i];
+
+    const predicateMatches = equation.predicate(toMatchOn);
+    if (predicateMatches) {
+      return equation.clause(parameters);
+    }
+  }
+
+  return undefined;
+
+  function pair(equations) {
+    const result = [];
+    for (let i = 0; i < equations.length; i+=2) {
+      const predicate = equations[i];
+      const clause = equations[i+1];
+      result.push({predicate, clause});
+    }
+    return result;
+  }
 }
