c884b97ce3cfe9a49534a30ec6828112d1ed3247
Alvaro Garcia <derecerca@gmail.com>
2016-11-04 21:32:58 +0000

    green

--- javascript/src/streem.js
+++ javascript/src/streem.js
@@ -1,3 +1,31 @@
 'use strict';
 
-module.exports = {};
+module.exports = {infiniteNaturals};
+
+class InfiniteNaturalStream {
+
+  constructor(value) {
+    this.value = value;
+  }
+
+  next() {
+    return new InfiniteNaturalStream(this.value + 1);
+  }
+
+  take(n) {
+    function take_(result, n) {
+      if (n === 0) {
+        return result;
+      } else {
+        result.push(this.value);
+        return result.concat(this.next().take(n-1));
+      }
+    }
+    take_ = take_.bind(this);
+    return take_([], n);
+  }
+}
+
+function infiniteNaturals() {
+  return new InfiniteNaturalStream(1);
+}
