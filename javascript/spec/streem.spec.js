b8b1a3990a103059653fb1484e2c7f87d562e192
Alvaro Garcia <derecerca@gmail.com>
2016-11-04 21:34:47 +0000

    Green: take can take 0 elements

--- javascript/spec/streem.spec.js
+++ javascript/spec/streem.spec.js
@@ -10,6 +10,7 @@ describe('Streems (or Streams)', () => {
   describe('Exercise 1 - take: a function to convert a Stream to a List, which will force its evaluation', () => {
     it('on an infinite stream', () => {
       expect(streem.infiniteNaturals().take(10)).to.eql([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]);
+      expect(streem.infiniteNaturals().take(0)).to.eql([]);
     });
   });
 });
