8b24a40d00cc59a30a01aa648f91446aae9c955f
Alvaro Garcia <derecerca@gmail.com>
2016-11-04 22:40:31 +0000

    Refactor: use extracted module

--- javascript/src/streem.js
+++ javascript/src/streem.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const match = require('./pattern_matching');
+
 module.exports = {infiniteNaturals};
 
 class InfiniteNaturalStream {
@@ -45,38 +47,4 @@ function infiniteNaturals() {
  */
 function funct(lambda) {
   return lambda.bind(this);
-}
-
-function match(parameters, ...equationParts) {
-  const equations = pair(equationParts);
-  for (let i = 0; i < equations.length; i++) {
-    const equation = equations[i];
-
-    const predicateMatches = equation.predicate(parameters);
-    if (predicateMatches) {
-      return equation.clause(parameters);
-    }
-  }
-
-  return undefined;
-
-  function pair(equations) {
-    if (Array.isArray(equations[0])) {
-      const result = [];
-      for (let i = 0; i < equations.length; i++) {
-        const predicate = equations[i][0];
-        const clause = equations[i][1];
-        result.push({predicate, clause});
-      }
-      return result;
-    }
-
-    const result = [];
-    for (let i = 0; i < equations.length; i+=2) {
-      const predicate = equations[i];
-      const clause = equations[i+1];
-      result.push({predicate, clause});
-    }
-    return result;
-  }
 }
