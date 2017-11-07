969a7da2aa91352986087d1b3625a488355ec69f
Carlos Morera de la Chica <carlos@codurance.com>
2017-11-07 07:03:01 +0000

    Add running balance

--- katas/BankKata/Haskell-Carlos&Christian/bank-kata/src/BankAccount.hs
+++ katas/BankKata/Haskell-Carlos&Christian/bank-kata/src/BankAccount.hs
@@ -2,22 +2,61 @@
 
 module BankAccount where
 
-newtype Amount = Amount Int deriving (Num, Show, Eq)
 type Transactions = [Transaction]
-data BankAccount = BankAccount Transactions deriving (Show, Eq)
-data Transaction = Transaction Amount deriving (Show, Eq)
+type BankAccount = Transactions
+type Statement = [(Transaction, RunningBalance)]
 
-deposit ::  BankAccount -> Int -> BankAccount
-deposit account amount = addTransaction account amount
+newtype Amount = Amount { unPositive :: Int } deriving (Num, Eq, Ord)
 
-withdraw ::  BankAccount -> Int -> BankAccount
-withdraw account amount = addTransaction account (-amount)
+newtype RunningBalance = RunningBalance Int deriving (Eq)
 
-addTransaction ::  BankAccount -> Int -> BankAccount
-addTransaction (BankAccount transactions) amount = BankAccount $ Transaction (Amount amount) : transactions
+data Transaction =
+    Deposit Amount
+  | Withdraw Amount
+  deriving (Eq, Ord)
 
-printStatement ::  BankAccount -> [String]
-printStatement (BankAccount transactions) = map toStatementLine transactions
-  where toStatementLine (Transaction (Amount amount))
-          | amount > 0 = show amount ++ " |"
-          | amount < 0 = "| " ++ show  (-amount)
+instance Show RunningBalance where
+  show (RunningBalance x) = show x
+
+instance Show Transaction where
+  show (Deposit x)  = show x
+  show (Withdraw x) = show x
+
+instance Show Amount where
+  show (Amount x) = show x
+
+toPositive :: Int -> Maybe Amount
+toPositive x
+  | x > 0     = Just $ Amount $ x
+  | otherwise = Nothing
+
+deposit ::  Amount -> BankAccount -> BankAccount
+deposit amount account = addTransaction (Deposit $ amount) account
+
+withdraw ::  Amount -> BankAccount -> BankAccount
+withdraw amount account = addTransaction (Withdraw $ amount) account
+
+addTransaction ::  Transaction -> BankAccount -> BankAccount
+addTransaction transaction account = account ++ [transaction]
+
+printStatement ::  Statement -> [String]
+printStatement = fmap toStatementLine
+  where toStatementLine :: (Transaction, RunningBalance) -> String
+        toStatementLine (deposit@(Deposit _),  runningBalance) = "   "        ++ show deposit ++   "  |       |  " ++ show runningBalance
+        toStatementLine (withdraw@(Withdraw _), runningBalance) = "        | " ++ show withdraw ++  "   |  "        ++ show runningBalance
+
+amount :: Transaction -> Int
+amount (Deposit x)  = unPositive x
+amount (Withdraw x) = unPositive $ -x
+
+makeStatement :: BankAccount -> Statement
+makeStatement account = zip (reverse account) (reverse balances)
+  where balances = fmap RunningBalance . scanl1 (+) . fmap amount $ account
+
+
+newtype All' a = All' { get :: Maybe a }
+
+instance (Monoid a) => Monoid (All' a) where
+  mempty = All' Nothing
+  All' (Just x) `mappend` All' (Just x') = All' $ Just $ x `mappend` x'
+  _ `mappend` _                        = All' Nothing
--- katas/BankKata/Haskell-Carlos&Christian/bank-kata/test/BankkataSpec.hs
+++ katas/BankKata/Haskell-Carlos&Christian/bank-kata/test/BankkataSpec.hs
@@ -1,12 +1,10 @@
 module BankkataSpec  (main, spec) where
 
 import           BankAccount
+import           Data.Maybe
+import           Data.Monoid
 import           Test.Hspec
 import           Test.QuickCheck
-
-main :: IO ()
-main = hspec spec
-
 -- Given a client makes a deposit of 1000 on 10-01-2012
 -- And a deposit of 2000 on 13-01-2012
 -- And a withdrawal of 500 on 14-01-2012
@@ -17,11 +15,11 @@ main = hspec spec
 -- 13/01/2012 || 2000.00 || || 3000.00
 -- 10/01/2012 || 1000.00 || || 1000.00
 
-aDeposit = Transaction $ Amount 123
-anotherDeposit = Transaction $ Amount 456
+aDeposit = Deposit $ Amount 100
+anotherDeposit = Deposit $ Amount 200
 
-aWithdraw = Transaction $ Amount (-123)
-anotherWithdraw = Transaction $ Amount (-456)
+aWithdraw = Withdraw $ Amount 100
+anotherWithdraw = Withdraw $ Amount 200
 
 spec :: Spec
 spec = do
@@ -29,14 +27,37 @@ spec = do
 
     describe "Deposit" $ do
       it "deposits" $ do
-        deposit (BankAccount []) 123 `shouldBe` BankAccount [Transaction (Amount 123)]
+        deposit 100 [] `shouldBe` [Deposit $ Amount 100]
 
     describe "Withdraw" $ do
       it "withdraws" $ do
-        withdraw (BankAccount []) 123 `shouldBe` BankAccount [Transaction (Amount (-123))]
+        withdraw 100 [] `shouldBe` [Withdraw $ Amount (100)]
 
-    describe "Format statement" $ do
-      it "Format deposits in debit colum" $ do
-        printStatement (BankAccount [aDeposit, anotherDeposit]) `shouldBe` ["123 |", "456 |"]
-      it "Format withdraws in credit colum" $ do
-        printStatement (BankAccount [aWithdraw, anotherWithdraw]) `shouldBe` ["| 123", "| 456"]
+    describe "Statement" $ do
+      it "creates statement" $ do
+        (makeStatement . deposit 100 . withdraw 100 . withdraw 200 . deposit 200 . deposit 300 $ []) `shouldBe`
+          [(Deposit  100, RunningBalance 300),
+           (Withdraw 100, RunningBalance 200),
+           (Withdraw 200, RunningBalance 300),
+           (Deposit  200, RunningBalance 500),
+           (Deposit  300, RunningBalance 300)
+          ]
+
+-- main :: IO ()
+-- main = hspec spec
+
+makeWithdraw :: Int -> Maybe (BankAccount -> BankAccount)
+makeWithdraw x = fmap withdraw $ (toPositive x)
+
+makeDeposit :: Int -> Maybe (BankAccount -> BankAccount)
+makeDeposit x = fmap deposit $ (toPositive x)
+
+main :: IO ()
+main = do
+  transactions <- return $ get $ (All' $ makeWithdraw 100) <> (All' $ makeDeposit 500) <> (All' $ makeDeposit 200)
+  account <- return $ transactions <*> Just []
+  statement <- return $ makeStatement <$> account
+  statementLines <- return $ fromMaybe [""] $ printStatement <$> statement
+  print " credit | debit | balance"
+  mapM_ print statementLines
+  return $ ()
