a6c838c71a9ab3dd2c5fefa20ebad762f6bd1d40
Carlos Morera de la Chica <carlos@codurance.com>
2017-11-11 18:28:38 +0000

    Improve Account creation interface

--- katas/BankKata/Haskell-Carlos&Christian/bank-kata/src/BankAccount.hs
+++ katas/BankKata/Haskell-Carlos&Christian/bank-kata/src/BankAccount.hs
@@ -1,19 +1,22 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE QuasiQuotes                #-}
 
 module BankAccount where
 
-type Transactions = [Transaction]
-type BankAccount = Transactions
-type Statement = [(Transaction, RunningBalance)]
+import           Data.Maybe
+import           Data.String.Interpolate
 
 newtype Amount = Amount { unPositive :: Int } deriving (Num, Eq, Ord)
-
 newtype RunningBalance = RunningBalance Int deriving (Eq)
 
 data Transaction =
     Deposit Amount
   | Withdraw Amount
   deriving (Eq, Ord)
+
+newtype BankAccount = BankAccount { transactions  :: [Transaction] } deriving (Show, Eq)
+
+type Statement = [(Transaction, RunningBalance)]
 
 instance Show RunningBalance where
   show (RunningBalance x) = show x
@@ -37,26 +40,37 @@ withdraw ::  Amount -> BankAccount -> BankAccount
 withdraw amount account = addTransaction (Withdraw $ amount) account
 
 addTransaction ::  Transaction -> BankAccount -> BankAccount
-addTransaction transaction account = account ++ [transaction]
+addTransaction transaction account = BankAccount $ transactions account ++ [transaction]
 
 printStatement ::  Statement -> [String]
-printStatement = fmap toStatementLine
-  where toStatementLine :: (Transaction, RunningBalance) -> String
-        toStatementLine (deposit@(Deposit _),  runningBalance) = "   "        ++ show deposit ++   "  |       |  " ++ show runningBalance
-        toStatementLine (withdraw@(Withdraw _), runningBalance) = "        | " ++ show withdraw ++  "   |  "        ++ show runningBalance
+printStatement statement = header ++  fmap toStatementLine statement
+  where header = [" credit | debit | balance"]
+        toStatementLine :: (Transaction, RunningBalance) -> String
+        toStatementLine (deposit@(Deposit _),  runningBalance)  = "   " ++ show deposit  ++  "  |  " ++     "   "     ++ "  |  " ++ show runningBalance
+        toStatementLine (withdraw@(Withdraw _), runningBalance) = "   " ++     "   "     ++  "  |  " ++ show withdraw ++ "  |  " ++ show runningBalance
 
 amount :: Transaction -> Int
 amount (Deposit x)  = unPositive x
 amount (Withdraw x) = unPositive $ -x
 
 makeStatement :: BankAccount -> Statement
-makeStatement account = zip (reverse account) (reverse balances)
-  where balances = fmap RunningBalance . scanl1 (+) . fmap amount $ account
+makeStatement account = zip (reverse . transactions $ account) (reverse balances)
+  where balances = fmap RunningBalance . scanl1 (+) . fmap amount $ transactions account
 
+makeWithdraw :: Int -> Maybe (BankAccount -> BankAccount)
+makeWithdraw x = fmap withdraw $ (toPositive x)
 
-newtype All' a = All' { get :: Maybe a }
+makeDeposit :: Int -> Maybe (BankAccount -> BankAccount)
+makeDeposit x = fmap deposit $ (toPositive x)
 
-instance (Monoid a) => Monoid (All' a) where
-  mempty = All' Nothing
-  All' (Just x) `mappend` All' (Just x') = All' $ Just $ x `mappend` x'
-  _ `mappend` _                        = All' Nothing
+makeAccount :: [Maybe (BankAccount -> BankAccount)] -> BankAccount
+makeAccount fs = compose (fromMaybe [] $ sequence fs) emptyAccount
+
+makeAccount' :: [Maybe (BankAccount -> BankAccount)] -> BankAccount
+makeAccount' fs = compose (catMaybes fs) emptyAccount
+
+emptyAccount :: BankAccount
+emptyAccount = BankAccount []
+
+compose :: [a -> a] -> a -> a
+compose = foldl (flip (.)) id
--- katas/BankKata/Haskell-Carlos&Christian/bank-kata/test/BankkataSpec.hs
+++ katas/BankKata/Haskell-Carlos&Christian/bank-kata/test/BankkataSpec.hs
@@ -1,7 +1,6 @@
 module BankkataSpec  (main, spec) where
 
 import           BankAccount
-import           Data.Maybe
 import           Data.Monoid
 import           Test.Hspec
 import           Test.QuickCheck
@@ -25,39 +24,50 @@ spec :: Spec
 spec = do
   describe "Bank Account" $ do
 
-    describe "Deposit" $ do
-      it "deposits" $ do
-        deposit 100 [] `shouldBe` [Deposit $ Amount 100]
+    it "Given a positive amount and an account, when adding a deposit, then adds deposit to account" $ do
+      makeDeposit 100 <*> Just emptyAccount `shouldBe` Just (BankAccount [Deposit $ Amount 100])
 
-    describe "Withdraw" $ do
-      it "withdraws" $ do
-        withdraw 100 [] `shouldBe` [Withdraw $ Amount (100)]
+    it "Given a positive amount and an account, when adding a withdraw, then adds withdraw to account" $ do
+      makeWithdraw 100 <*>  Just emptyAccount `shouldBe` Just (BankAccount [Withdraw $ Amount (100)])
 
-    describe "Statement" $ do
-      it "creates statement" $ do
-        (makeStatement . deposit 100 . withdraw 100 . withdraw 200 . deposit 200 . deposit 300 $ []) `shouldBe`
+    it "Given a negative amount and an account, when adding a deposit, then returns Nothing" $ do
+      makeDeposit (-100) <*>  Just emptyAccount `shouldBe` Nothing
+
+    it "Given a negative amount and an account, when adding a withdraw, then returns Nothing" $ do
+      makeWithdraw (-100) <*>  Just emptyAccount `shouldBe` Nothing
+
+    it "Given a list of transactions with invalid entries can create account discarting invalid transactions" $ do
+      makeAccount' [makeWithdraw (-100), makeDeposit 200, makeDeposit 300, makeWithdraw 600] `shouldBe`
+        BankAccount [Deposit 200, Deposit 300, Withdraw 600]
+    it "Given a list of transactions with invalid entries can create account discarting all transactions" $ do
+      makeAccount [makeWithdraw (-100), makeDeposit 200, makeDeposit 300, makeWithdraw 600] `shouldBe`
+        BankAccount []
+
+    it "Given an account creates a statement that contains the transactions and the running balance in reverse order" $ do
+        account <- return $ makeAccount $ [makeDeposit 300, makeDeposit 200, makeWithdraw 200, makeWithdraw 100, makeDeposit 100]
+        makeStatement account `shouldBe`
           [(Deposit  100, RunningBalance 300),
            (Withdraw 100, RunningBalance 200),
            (Withdraw 200, RunningBalance 300),
            (Deposit  200, RunningBalance 500),
            (Deposit  300, RunningBalance 300)
           ]
+    it "Given an statement can format it" $ do
+        account <- return $ makeAccount $ [makeDeposit 300, makeDeposit 200, makeWithdraw 200, makeWithdraw 100, makeDeposit 100]
+        statementLines <- return $ printStatement . makeStatement $ account
+        statementLines `shouldBe`
+          [" credit | debit | balance",
+           "   100  |       |  300",
+           "        |  100  |  200",
+           "        |  200  |  300",
+           "   200  |       |  500",
+           "   300  |       |  300"
+          ]
 
--- main :: IO ()
--- main = hspec spec
-
-makeWithdraw :: Int -> Maybe (BankAccount -> BankAccount)
-makeWithdraw x = fmap withdraw $ (toPositive x)
-
-makeDeposit :: Int -> Maybe (BankAccount -> BankAccount)
-makeDeposit x = fmap deposit $ (toPositive x)
+statementLines ::  [String]
+statementLines = printStatement . makeStatement . makeAccount' $ [makeWithdraw (-100), makeDeposit 200, makeDeposit 300, makeWithdraw 600]
 
 main :: IO ()
 main = do
-  transactions <- return $ get $ (All' $ makeWithdraw 100) <> (All' $ makeDeposit 500) <> (All' $ makeDeposit 200)
-  account <- return $ transactions <*> Just []
-  statement <- return $ makeStatement <$> account
-  statementLines <- return $ fromMaybe [""] $ printStatement <$> statement
-  print " credit | debit | balance"
-  mapM_ print statementLines
-  return $ ()
+-- mapM_ print statementLines
+  hspec spec
