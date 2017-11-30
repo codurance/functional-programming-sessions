543717882eb11950e3fed1163ae75f356e1dca5e
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-30 19:34:00 +0000

    raw free monads implementation

--- katas/BankKata/Scala - Sergio & Andrzej/fp-bank-kata/build.sbt
+++ katas/BankKata/Scala - Sergio & Andrzej/fp-bank-kata/build.sbt
@@ -4,6 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.0.0-RC1"
+libraryDependencies += "org.typelevel" %% "cats-free" % "1.0.0-RC1"
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
 
---
+++ katas/BankKata/Scala - Sergio & Andrzej/fp-bank-kata/src/main/scala-2.12/BankKataFreeMonads.scala
@@ -0,0 +1,79 @@
+import cats.free.Free
+import cats.{Id, ~>}
+
+import scala.collection.mutable
+
+object BankKataFreeMonads {
+
+    case class Account()
+
+    sealed trait TransactionOp[A]
+    case class Deposit(amount: Int) extends TransactionOp[Unit]
+    case class Withdrawal(amount: Int) extends TransactionOp[Unit]
+    case class PrintBankStatement() extends TransactionOp[Unit]
+
+    implicit def liftTransactionOp[A](t: TransactionOp[A]): Free[TransactionOp, A] = Free.liftF(t)
+
+    def program: Free[TransactionOp, Unit] = for {
+        _ <- Deposit(50)
+        _ <- Withdrawal(30)
+        _ <- Deposit(90)
+        _ <- Withdrawal(40)
+        _ <- PrintBankStatement()
+    } yield ()
+
+    def transactionInterpreter = new (TransactionOp ~> Id) {
+        case class StatementLine(credit: Int, debit: Int, balance: Int)
+
+        val transactions: mutable.ListBuffer[Int] = mutable.ListBuffer[Int]()
+
+        override def apply[A](fa: TransactionOp[A]) = fa match {
+            case Deposit(amount) => {
+                transactions += amount
+                ()
+            }
+            case Withdrawal(amount) => {
+                transactions += -amount
+                ()
+            }
+            case PrintBankStatement() => {
+                val runnningBalances = transactions
+                        .scan(0)(_ + _)
+                val transactionsWithBalances = transactions
+                        .zip(runnningBalances.drop(1))
+                        .reverse
+
+                val lines = transactionsWithBalances
+                        .map(t => buildStatementLine(t._1, t._2))
+                        .map(formatStatementLine)
+
+                println("date || credit || debit || balance")
+                lines.foreach(println)
+            }
+        }
+
+        private def buildStatementLine(amount: Int, balance: Int): StatementLine = {
+            val credit = if (amount > 0) amount else 0
+            val debit = if (amount < 0) amount else 0
+
+            StatementLine(credit, debit, balance)
+        }
+
+        private def formatStatementLine(statementLine: StatementLine): String = {
+            val credit = if (statementLine.credit == 0) "" else statementLine.credit
+            val debit = if (statementLine.debit == 0) "" else statementLine.debit
+
+            s"$credit || $debit || ${statementLine.balance}"
+        }
+
+    }
+
+    def interpreter = transactionInterpreter
+
+    // Try test interpreter
+
+    def main(args: Array[String]) {
+        program.foldMap(interpreter)
+    }
+
+}
