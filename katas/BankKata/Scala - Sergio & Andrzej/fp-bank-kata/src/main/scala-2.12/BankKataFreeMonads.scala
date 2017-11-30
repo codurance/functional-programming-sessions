fb69021b157677caf6eef37d7765a0d8bbdd06b8
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-11-30 20:34:00 +0000

    add dates (without format yet)

--- katas/BankKata/Scala - Sergio & Andrzej/fp-bank-kata/src/main/scala-2.12/BankKataFreeMonads.scala
+++ katas/BankKata/Scala - Sergio & Andrzej/fp-bank-kata/src/main/scala-2.12/BankKataFreeMonads.scala
@@ -6,41 +6,50 @@ import scala.collection.mutable
 object BankKataFreeMonads {
 
     case class Account()
+    case class StatementLine(credit: Int = 0, debit: Int = 0, balance: Int = 0, date: Long = 0)
+    case class Transaction(amount: Int, date: Long)
 
     sealed trait TransactionOp[A]
-    case class Deposit(amount: Int) extends TransactionOp[Unit]
-    case class Withdrawal(amount: Int) extends TransactionOp[Unit]
+    case class MakeDeposit(amount: Int, date: Long) extends TransactionOp[Unit]
+    case class MakeWithdrawal(amount: Int, date: Long) extends TransactionOp[Unit]
     case class PrintBankStatement() extends TransactionOp[Unit]
 
     implicit def liftTransactionOp[A](t: TransactionOp[A]): Free[TransactionOp, A] = Free.liftF(t)
 
     def program: Free[TransactionOp, Unit] = for {
-        _ <- Deposit(50)
-        _ <- Withdrawal(30)
-        _ <- Deposit(90)
-        _ <- Withdrawal(40)
+        _ <- MakeDeposit(50, now)
+        _ <- MakeWithdrawal(30, now)
+        _ <- MakeDeposit(90, now)
+        _ <- MakeWithdrawal(40, now)
         _ <- PrintBankStatement()
     } yield ()
 
+    private def now = System.currentTimeMillis()
+
     def transactionInterpreter = new (TransactionOp ~> Id) {
-        val transactions: mutable.ListBuffer[Int] = mutable.ListBuffer[Int]()
+        val transactions: mutable.ListBuffer[Transaction] = mutable.ListBuffer[Transaction]()
 
         override def apply[A](fa: TransactionOp[A]) = fa match {
-            case Deposit(amount) => {
-                transactions += amount
+            case MakeDeposit(amount, date) => {
+                transactions += Transaction(amount, date)
                 ()
             }
-            case Withdrawal(amount) => {
-                transactions += -amount
+            case MakeWithdrawal(amount, date) => {
+                transactions += Transaction(-amount, date)
                 ()
             }
             case PrintBankStatement() => {
                 val statementLines = transactions
-                        .scan(0)(_ + _)
+                        .scanLeft(StatementLine())((acc, curr) => {
+                            val amount = curr.amount
+                            val date = curr.date
+                            val credit = if (amount > 0) amount else 0
+                            val debit = if (amount < 0) amount else 0
+                            StatementLine(credit, debit, amount + acc.balance, date)
+                        })
                         .drop(1)
-                        .zip(transactions)
                         .reverse
-                        .map(t => formatStatementLine(t._1, t._2))
+                        .map(formatStatementLine)
 
                 println("date || credit || debit || balance")
                 statementLines.foreach(println)
@@ -48,11 +57,13 @@ object BankKataFreeMonads {
             }
         }
 
-        private def formatStatementLine(balance: Int, amount: Int): String = {
-            val credit = if (amount > 0) amount else ""
-            val debit = if (amount < 0) amount else ""
+        private def formatStatementLine(statementLine: StatementLine): String = {
+            val amountToStr = (amount: Int) => if (amount == 0) "" else amount
 
-            s"$credit || $debit || $balance"
+            val credit = amountToStr(statementLine.credit)
+            val debit = amountToStr(statementLine.debit)
+
+            s"${statementLine.date} || $credit || $debit || ${statementLine.balance}"
         }
 
     }
